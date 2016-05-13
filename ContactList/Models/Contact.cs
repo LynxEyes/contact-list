390c464a7bf48f378cdba4e1cea6e9baa397e900
Shemahmforash <crosao@equalexperts.com>
2016-05-13 18:32:58 +0100

    Add first sketch of model

---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList.Models {
+    public class Contact {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set;  }
+
+        public Contact(string Name, string Email, string Mobile) {
+            this.Email = Email;
+            this.Name = Name;
+            this.Mobile = Mobile;
+        }
+    }
+}
