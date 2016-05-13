e431f120a740db30a03e967e7c1bd2f04ae2e907
IEUser <IEUser@MSEDGEWIN10>
2016-05-13 12:34:00 +0100

    Add project files.

---
+++ ContactList/Views/DetailPage.xaml.cs
@@ -0,0 +1,13 @@
+using ContactList.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ContactList.Views {
+  public sealed partial class DetailPage : Page {
+    public DetailPage() {
+      InitializeComponent();
+      NavigationCacheMode = NavigationCacheMode.Disabled;
+    }
+  }
+}
+
---
+++ ContactList/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using ContactList.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace ContactList.Views {
+  public sealed partial class MainPage : Page {
+    public MainPage() {
+      InitializeComponent();
+      NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+    }
+  }
+}
